cec7fd28d4320650f49f6f779cbfefd0ba9bf96e
Adam Struthers <acstruthers@gmail.com>
2019-03-10 13:02:27 +1100

    CHANGE TO USE XoRoShiRo128PlusRandom RANDOM NUMBER GENERATOR
    - Modified Properties to use the XoRoShiRo128PlusRandom pseudo-random
      number genreator. It's faster than java.util.Random and has a better
      distribution.
      SOURCES:
      https://stackoverflow.com/questions/29193371/fast-real-valued-random-generator-in-java
      http://dsiutils.di.unimi.it/docs/it/unimi/dsi/util/XoRoShiRo128PlusRandom.html

--- xyz.struthers.rhul.ham/src/main/java/xyz/struthers/rhul/ham/config/Properties.java
+++ xyz.struthers.rhul.ham/src/main/java/xyz/struthers/rhul/ham/config/Properties.java
@@ -8,7 +8,7 @@ import java.util.Random;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-import ec.util.MersenneTwisterFast;
+import it.unimi.dsi.util.XoRoShiRo128PlusRandom;
 
 /**
  * @author Adam Struthers
@@ -21,23 +21,26 @@ public class Properties {
 	// static config constants
 	/*
 	 * Use a classpath resource loader instead. One of these two options:
-	 * InputStream in = Foobar.class.getClassLoader().getResourceAsStream("data/ABS/1270.0.55.001_AbsMeshblock/MB_2016_ACT.csv");
-	 * InputStream in = Foobar.class.getResourceAsStream("/data/ABS/1270.0.55.001_AbsMeshblock/MB_2016_ACT.csv");
+	 * InputStream in = Foobar.class.getClassLoader().getResourceAsStream(
+	 * "data/ABS/1270.0.55.001_AbsMeshblock/MB_2016_ACT.csv"); InputStream in =
+	 * Foobar.class.getResourceAsStream(
+	 * "/data/ABS/1270.0.55.001_AbsMeshblock/MB_2016_ACT.csv");
 	 */
 	public static final String HOME_DIRECTORY = "F:\\OneDrive\\Dissertation\\STS\\xyz.struthers.rhul.ham"; // lappy
-	//public static final String HOME_DIRECTORY = "D:\\Git\\compham\\xyz.struthers.rhul.ham"; // NUC
+	// public static final String HOME_DIRECTORY =
+	// "D:\\Git\\compham\\xyz.struthers.rhul.ham"; // NUC
 	public static final String RESOURCE_DIRECTORY = HOME_DIRECTORY + "\\src\\main\\resources";
-	
+
 	public static final double SUPERANNUATION_RATE = 0.095; // 9.5%
 	public static final long RANDOM_SEED = 20190315L;
-	
+
 	// static data fields
 	public static int peoplePerAgent = 1; // change to 1000 if 1 is too slow.
 	public static int totalPopulationAU = 25000000;
 
 	// data fields
 	public Random random;
-	
+
 	private Properties() {
 		super();
 	}
@@ -50,8 +53,7 @@ public class Properties {
 	}
 
 	/**
-	 * @param peoplePerAgent
-	 *            the peoplePerAgent to set
+	 * @param peoplePerAgent the peoplePerAgent to set
 	 */
 	public static void setPeoplePerAgent(int peoplePerAgent) {
 		Properties.peoplePerAgent = peoplePerAgent;
@@ -65,22 +67,29 @@ public class Properties {
 	}
 
 	/**
-	 * @param totalPopulationAU
-	 *            the totalPopulationAU to set
+	 * @param totalPopulationAU the totalPopulationAU to set
 	 */
 	public static void setTotalPopulationAU(int totalPopulationAU) {
 		Properties.totalPopulationAU = totalPopulationAU;
 	}
 
 	/**
+	 * The XoRoShiRo128PlusRandom random number generator. It is a sub-class of
+	 * Random, so will work anywhere that Random will, but it's faster and with a
+	 * better statistical distribution.
+	 * 
+	 * SOURCES:<br>
+	 * https://stackoverflow.com/questions/29193371/fast-real-valued-random-generator-in-java<br>
+	 * http://dsiutils.di.unimi.it/docs/it/unimi/dsi/util/XoRoShiRo128PlusRandom.html<br>
+	 * 
 	 * @return the random
 	 */
 	public Random getRandom() {
 		if (this.random == null) {
-			this.random = new Random(RANDOM_SEED);
+			//this.random = new Random(RANDOM_SEED);
+			this.random = new XoRoShiRo128PlusRandom(RANDOM_SEED);
 		}
 		return random;
 	}
-	
-	
+
 }
