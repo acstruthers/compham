f6fdc2b2f903d093c37d75267c2240685a3c355c
Adam Struthers <acstruthers@gmail.com>
2019-04-14 15:06:54 +1000

    CREATE RMI REGISTRY IN CODE NOT ON COMMAND Lines
    - Makes it easier because fewer steps to run the code.

--- xyz.struthers.rhul.ham/src/main/java/xyz/struthers/rmi/HelloServer.java
+++ xyz.struthers.rhul.ham/src/main/java/xyz/struthers/rmi/HelloServer.java
@@ -16,32 +16,39 @@ public class HelloServer implements IHello {
 
 	public static final String HOST = "Adam-E590";
 	public static final int PORT = 1099;
-	
+
 	public HelloServer() {
 		super();
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see xyz.struthers.rmi.IHello#sayHello(java.lang.String)
 	 */
 	@Override
 	public String sayHello(String input) throws RemoteException {
 		return input + ", world!";
 	}
-	
-	public static void main (String args[]) {
+
+	public static void main(String args[]) {
 		try {
+			// Create RMI Registry
+			System.setProperty("java.rmi.server.hostname", HOST);
+			LocateRegistry.createRegistry(PORT);
+
+			// register object
 			HelloServer obj = new HelloServer();
 			IHello stub = (IHello) UnicastRemoteObject.exportObject(obj, 0);
-			
-			// Bind the remote object's stub in the registry
-            Registry registry = LocateRegistry.getRegistry(HOST, PORT);
-            registry.bind("Hello", stub);
 
-            System.err.println("Server ready");
+			// Bind the remote object's stub in the registry
+			Registry registry = LocateRegistry.getRegistry(HOST, PORT);
+			registry.bind("Hello", stub);
+
+			System.err.println("Server ready");
 		} catch (Exception e) {
-            System.err.println("HelloServer exception: " + e.toString());
-            e.printStackTrace();
-        }
+			System.err.println("HelloServer exception: " + e.toString());
+			e.printStackTrace();
+		}
 	}
 }
