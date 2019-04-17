70ed07d091e7134b9d036e2d99b33362cc53cf01
Adam Struthers <acstruthers@gmail.com>
2019-04-17 22:30:19 +1000

    ADDING MORE DATA TYPES TO KRYONET REGISTRATION
    - List, ArrayList, Float, Integer, int.
    - It caused a network error before, so hopefully this will fix it.

--- xyz.struthers.rhul.ham/src/main/java/xyz/struthers/rhul/ham/KryonetNetwork.java
+++ xyz.struthers.rhul.ham/src/main/java/xyz/struthers/rhul/ham/KryonetNetwork.java
@@ -1,5 +1,8 @@
 package xyz.struthers.rhul.ham;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryonet.EndPoint;
 
@@ -9,7 +12,7 @@ import xyz.struthers.rhul.ham.process.ClearingPaymentOutputs;
 //This class is a convenient place to keep things common to both the client and server.
 public class KryonetNetwork {
 
-	//public static final String IPADDRESS_SERVER = "192.168.1.74";
+	// public static final String IPADDRESS_SERVER = "192.168.1.74";
 	public static final String IPADDRESS_SERVER = "169.254.58.222";
 	public static final int NETWORK_PORT_TCP = 1099; // 54555 default value
 	public static final int NETWORK_PORT_UDP = 54777;
@@ -24,5 +27,10 @@ public class KryonetNetwork {
 		Kryo kryo = endPoint.getKryo();
 		kryo.register(ClearingPaymentInputs.class);
 		kryo.register(ClearingPaymentOutputs.class);
+		kryo.register(List.class);
+		kryo.register(ArrayList.class);
+		kryo.register(Float.class);
+		kryo.register(Integer.class);
+		kryo.register(int.class);
 	}
 }
