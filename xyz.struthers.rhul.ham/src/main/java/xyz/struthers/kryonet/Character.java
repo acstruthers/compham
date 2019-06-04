d347048425550606934ef96d076f6b26a06247f7
Adam Struthers <acstruthers@gmail.com>
2019-06-05 08:20:07 +1000

    REMOVE DEPRECATED CODE (KRYONET, RMI, MATRIX CPV IMPLEMENTATION)
    - Removes Kryo, Kryonet and Java RMI code.
    - Removes deprecated matrix implementation of CPV.
    - Reduces code by 54 pages, from 672 A4 pages down to 618 pages.

--- xyz.struthers.rhul.ham/src/main/java/xyz/struthers/kryonet/Character.java
+++
@@ -1,7 +0,0 @@
-package xyz.struthers.kryonet;
-
-public class Character {
-	public String name;
-	public String otherStuff;
-	public int id, x, y;
-}
--- xyz.struthers.rhul.ham/src/main/java/xyz/struthers/kryonet/KryonetRmiHelloInterface.java
+++
@@ -1,7 +0,0 @@
-package xyz.struthers.kryonet;
-
-import java.rmi.RemoteException;
-
-public interface KryonetRmiHelloInterface {
-	String sayHello(String input) throws RemoteException;
-}
